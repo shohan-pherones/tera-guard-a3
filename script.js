9e130330b5c01a97cd73af2d31a86f93349a9a28
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-29 23:28:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/////////////////////////////////////////////////////////////
+// Elements
+/////////////////////////////////////////////////////////////
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelector(".nav__links");
+const header = document.querySelector(".header");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const allSections = document.querySelectorAll(".section");
+const section1 = document.querySelector("#section--1");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContent = document.querySelectorAll(".operations__content");
+const cookieBody = document.querySelector(".cookie");
+const cookieCloseBtn = document.querySelector(".cookie__close");
+const imgTargets = document.querySelectorAll("img[data-src]");
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
