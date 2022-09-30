c218c00cc72ffba9efd550fe63fee1139fbac6af
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-10-01 00:20:15 +0600

    section reveal

--- script.js
+++ script.js
@@ -25,3 +25,47 @@ const slider = document.querySelector(".slider");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
 const dotContainer = document.querySelector(".dots");
+
+// Cookie
+cookieCloseBtn.addEventListener("click", function () {
+  cookieBody.classList.add("hidden");
+  cookieBody.style.bottom = "-12rem";
+});
+
+// Stick navbar
+// nav's height
+const navHeight = nav.getBoundingClientRect().height;
+
+function sticky(entries) {
+  const entry = entries[0];
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(sticky, {
+  root: null, // viewport
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+//  reveal section
+
+function revealSection(entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+}
+
+const sectionObs = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.3,
+});
+
+allSections.forEach((section) => {
+  sectionObs.observe(section);
+  section.classList.add("section--hidden");
+});
