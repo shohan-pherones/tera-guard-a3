a546729d158ce14da23619936222c1eb8b74e3e0
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-10-08 21:44:03 +0600

    slider added

--- script.js
+++ script.js
@@ -153,32 +153,62 @@ const maxSlide = slides.length - 1;
 // DOts
 function creatingDots() {
   slides.forEach((_, i) => {
-    const dot = `<button class="dots__dot" data-slide="${i}></button>`;
+    const dot = `<button class="dots__dot" data-slide="${i}"></button>`;
     dotContainer.insertAdjacentHTML("beforeend", dot);
   });
 }
+
 creatingDots();
 
-function changeSlide(cs) {
+// Activate dots
+function activateDots(slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+}
+
+activateDots(0);
+
+function updateSlide(cs) {
   slides.forEach(
     (sl, i) => (sl.style.transform = `translateX(${100 * (i - cs)}%)`)
   );
 }
 
-changeSlide(0);
+updateSlide(0);
 
 function previousSlide() {
   if (currentSlide === 0) currentSlide = maxSlide;
   else currentSlide--;
-  changeSlide(currentSlide);
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
 }
 
-function nextslide() {
+function nextSlide() {
   if (currentSlide === maxSlide) currentSlide = 0;
   else currentSlide++;
-  changeSlide(currentSlide);
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
 }
+
+// dots handler
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    activateDots(e.target.dataset.slide);
+    updateSlide(e.target.dataset.slide);
+  }
+});
 
 // button handles
 btnLeft.addEventListener("click", previousSlide);
-btnRight.addEventListener("click", nextslide);
+btnRight.addEventListener("click", nextSlide);
+
+// keyboard
+document.addEventListener("keydown", (e) => {
+  e.key === "ArrowLeft" && previousSlide();
+  e.key === "ArrowRight" && nextSlide();
+});
